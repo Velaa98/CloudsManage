83fd58d286f7f9792c29302144ff16127a06c3c9
José Manuel Vela Fernández <vela.1998@gmail.com>
2018-06-13 14:45:22 +0200

    add funciones

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, session, request, jsonify, render_template
 from flask_oauthlib.client import OAuth, OAuthException
 from logging import Logger
-import uuid, json, requests, os
+import uuid, json, requests, os, funciones
 
 app = Flask(__name__)
 
@@ -37,43 +37,8 @@ def preview():
 @app.route('/tree')
 def tree():
 	dic = microsoft.get('drive/root/children').data
-	tree = []
-	for elem in dic["value"]:
-		# Si el path es root entra en el elemento 0 de tree.
-		if elem["parentReference"]["path"] == "/drive/root:":
-			# Si La lista tree aún no tiene elementos crea el primero. 
-			if len(tree) == 0:
-				tree.append({})
-			
-			# Si hay una carpeta en el nivel,
-			if "folder" in elem:
-				# Y aún no existe el diccionario folders, lo crea y añade a la lista uno nuevo con el nombre de la carpeta.
-				if "folders" not in tree[0]:
-					tree[0]["folders"]=[]
-					tree[0]["folders"].append(dict(name=elem["name"]))
-				# si existe añade a la lista de folders un nuevo diccionario con el nombre de la carpeta.
-				else:
-					tree[0]["folders"].append(dict(name=elem["name"]))
-				# si la carpeta tiene carpetas dentro:
-				if elem["folder"]["childCount"] > 0:
-					# Se recorren las carpetas que hay en el nivel 0
-					for i in tree[0]["folders"]:
-						# Cuando la carpeta sea la que estamos tratando,
-						if i["name"] == elem["name"]:
-							# Añade un nuevo valor al diccionario con formato: "NumCarpetas"= X
-							i["childCount"]=elem["folder"]["childCount"]
-
-			# Si hay un fichero en el nivel,
-			if "file" in elem:
-				# Y aún no existe el diccionario files, lo crea y añade a la lista uno nuevo con el nombre del fichero.
-				if "files" not in tree[0]:
-					tree[0]["files"]=[]
-					tree[0]["files"].append(dict(name=elem["name"]))	
-				# si existe añade a la lista de files un nuevo diccionario con el nombre del fichero.
-				else:
-					tree[0]["files"].append(dict(name=elem["name"]))
-	return render_template("tree.html", tree=str(tree))
-
+	return render_template("tree.html", tree=str(funciones.FormarArbol(dic)))
+	
 ## Subida simultanea
 @app.route('/upload')
 def upload():
